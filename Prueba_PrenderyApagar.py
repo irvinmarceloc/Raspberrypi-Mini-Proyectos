ba7e432a84e23b81f1683ca0b785766f2e0ef15d
irvinmarceloc <irvinmarceloc@gmail.com>
2021-03-29 16:25:35 -0400

    Prueba de Funcionamiento

--- Prueba_PrenderyApagar.py
+++ Prueba_PrenderyApagar.py
@@ -1,11 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+def setupRasbperry():
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(2, GPIO.OUT)
 
-while True:
-   GPIO.output(2, True)
-   time.sleep(20)
-   GPIO.output(2, False)
-   time.sleep(25)
+
+if __name__ == "__main__":
+    while (True):
+        setupRasbperry()
+	GPIO.output(2, True)
+	time.sleep(20)
+	GPIO.output(2, False)
+	time.sleep(25)
+
+
