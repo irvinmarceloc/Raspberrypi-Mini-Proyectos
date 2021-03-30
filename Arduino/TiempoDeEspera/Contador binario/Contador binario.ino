175b6e56b5d6b31d8dd78d70b3a085873301a245
irvinmarceloc <irvinmarceloc@gmail.com>
2021-03-30 12:57:30 -0400

    Contador Binario

--- Arduino/TiempoDeEspera/Contador binario/Contador binario.ino
+++ Arduino/TiempoDeEspera/Contador binario/Contador binario.ino
@@ -3,45 +3,51 @@ void setup() {
   for ( i = 10; i <14 ; i++)  pinMode(i, OUTPUT); 
   // sets the digital pin 13 as output
 }
-int i=0, i2=0, i3=0, i4=0;
+int i=0, i1=0, i2=0, i3=0, i4=0;
 
 void loop() {
   
-  if (i%2 = 0) digitalWrite(13, HIGH);
-  else digitalWrite(13, LOW);
+  if (i1<1) 
+  {
+    i1++;
+    digitalWrite(12, LOW);
+  }else {
+    i1 = 0;
+    digitalWrite(12, HIGH);
+  }
 
   if (i2<2) 
   {
     i2++;
-    digitalWrite(12, LOW);
+    digitalWrite(12, HIGH);
   }else {
     i2 = 0;
-    digitalWrite(12, HIGH);
+    digitalWrite(12, LOW);
   }
   
   if (i2<4) 
   {
     i3++;
-    digitalWrite(10, LOW);
+    digitalWrite(10, HIGH);
     
   }else {
     i3 = 0;
-    digitalWrite(10, HIGH);
+    digitalWrite(10, LOW);
   }
 
   if (i4<4) 
   {
     i4++;
-    digitalWrite(10, LOW);
+    digitalWrite(10, HIGH);
     
   }else {
     i4 = 0;
-    digitalWrite(10, HIGH);
+    digitalWrite(10, LOW);
   }
 
   if (i = 15) i=-1;
   i++;
-  
+ 
   delay(3000);            // waits for a second
   
 }
